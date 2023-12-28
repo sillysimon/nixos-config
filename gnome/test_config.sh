267e50297b95f5dc524d2139100ab79654393627
Simon Waidele <dont@example.com>
2023-12-28 14:47:42 +0100

    added a test config script

--- gnome/test_config.sh
+++ gnome/test_config.sh
@@ -1,5 +1,5 @@
 #!/run/current-system/sw/bin/bash
 # Script for quick loading and building the nixos config
 sudo cp -r ./* /etc/nixos/
-sudo nixos-rebuild test --flake /etc/nixos#default --dry-run
+sudo nixos-rebuild test --flake /etc/nixos#default
 
