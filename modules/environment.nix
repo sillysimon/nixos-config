44f243c3b2765497f81cc5feadb9a242106303b7
Simon Waidele <dont@example.com>
2024-03-09 17:59:22 +0100

    added appimage support

--- modules/environment.nix
+++ modules/environment.nix
@@ -2,19 +2,33 @@
 {
   #environment specific configuration (like fonts)
   environment.systemPackages = [
+    #fonts
     pkgs.nerdfonts
     pkgs.corefonts
     pkgs.monaspace
     pkgs.comic-mono
+    # fun programs
     pkgs.sl
     pkgs.cmatrix
     pkgs.cowsay
     pkgs.lolcat
     pkgs.fortune
+    # system utilities
+    pkgs.appimage-run
   ];
   #allow for dynamically linked binaries
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
-    # dynamic libraries go here (not in the system packages)
+    # dynamic link libraries go here (not in the system packages)
   ];
+
+  #enable AppImage support
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
 }
