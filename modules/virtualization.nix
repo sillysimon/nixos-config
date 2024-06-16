7911332c606a074c2c3831b14eab506a9a38189e
Simon Waidele <kontakt@simon-waidele.de>
2024-06-16 18:22:26 +0200

    added kvm support

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -2,4 +2,11 @@
 {
   # this module enables virtualization capabilities such as containers and virtual machines
   virtualisation.podman.enable = true;
+  virtualisation.libvirtd.enable = true;
+  environment.systemPackages = [
+    ## Packages for kvm virtualization
+    pkgs.qemu
+    pkgs.virt-manager
+    pkgs.quickemu
+];
 }
